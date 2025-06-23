7d65c606d655ffc748f1b89d7fe3af229f3f7576
DangHuan <188451334@qq.com>
2025-06-23 13:58:43 +0800

    Update style.js

--- themes/matery/style.js
+++ themes/matery/style.js
@@ -44,12 +44,12 @@ const Style = () => {
       }
 
       ::-webkit-scrollbar-thumb {
-        background-color: #4338ca;
+        background-color: #00BDD6;
       }
 
       * {
         scrollbar-width: thin;
-        scrollbar-color: #4338ca transparent;
+        scrollbar-color: #00BDD6 transparent;
       }
     `}</style>
   )
