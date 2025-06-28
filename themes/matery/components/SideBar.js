c7900b4bd439c61c04113c7f66ae2772427410d0
DangHuan <188451334@qq.com>
2025-06-28 13:09:39 +0800

    Update SideBar.js

--- themes/matery/components/SideBar.js
+++ themes/matery/components/SideBar.js
@@ -16,7 +16,7 @@ const SideBar = (props) => {
       <div id='side-bar'>
           <div className="mh-48 w-full bg-indigo-700">
               <div className='mx-5 pt-6 pb-2'>
-                  <LazyImage src={siteInfo?.icon} className='cursor-pointer rounded-full' width={80} alt={siteConfig('AUTHOR')} href='/'/>
+                  <LazyImage src={siteInfo?.icon} className='cursor-pointer rounded-full' width={80} alt={siteConfig('AUTHOR')} href="/"/>
                   <div className='text-white text-xl my-1'>{siteConfig('TITLE')}</div>
                   <div className='text-xs my-1 text-gray-300'>{siteConfig('DESCRIPTION')}</div>
               </div>
